d89f6fce533510b8484018302a45b10d180a2269
CarrascoEdgar <edgar.prado-guajardo@outlook.com>
2025-05-21 12:39:27 -0700

    project 6 completed up to part 4

---
+++ scripts/validation.js
@@ -0,0 +1,41 @@
+const showInputError = (formEl, inputEl, errorMsg) => {
+  const errorMsgEl = document.querySelector(`#${inputEl.id}-error`);
+  errorMsgEl.textContent = errorMsg;
+
+};
+
+const hideInputError = (formEl, inputEl, errorMsg) => {
+  const errorMsgEl = document.querySelector (`#${inputEl.id}-error`);
+  errorMsgEl.textContent ="";
+}
+
+const checkInputValidity = (formEl, inputEl) => {
+  if (!inputEl.validity.valid) {
+    showInputError(formEl, inputEl, inputEl.validationMessage);
+  } else {
+    hideInputError(formEl, inputEl);
+  }
+}
+
+const setEventListeners = (formEl) => {
+   const inputList = formEl.querySelectorAll(".modal__input");
+   const buttonElement = formEl.querySelector(".modal__submit-button");
+
+
+
+  inputList.forEach((inputElement) => {
+    inputElement.addEventListener("input", function () {
+      checkInputValidity(formEl, inputElement);
+      //toggleButtonState(inputList, buttonElement);
+    });
+  });
+};
+
+const enableValidation = () => {
+  const formList = document.querySelectorAll(".modal__form");
+  formList.forEach((formEl) => {
+    setEventListeners(formEl);
+  })
+}
+
+enableValidation();
